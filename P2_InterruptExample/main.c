87fc060caf9292385dd957247c0020d94f7bdb3b
stephens2633 <stephens2633@gmail.com>
2018-02-06 22:32:47 -0500

    changed interrupt example to use smclk

--- P2_InterruptExample/main.c
+++ P2_InterruptExample/main.c
@@ -7,8 +7,6 @@ static unsigned int t1;
 __interrupt void CCR0_ISR(void)
 {
     TA0CTL &= ~TAIFG;               //clear timer A interrupt flag
-
-
     P1OUT ^= G_LED;                   //Toggle G_LED
 }
 
@@ -16,20 +14,18 @@ void main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;           //watchdog
 
-
-
     P1DIR |= G_LED;                       // Set G_LED pin -> Output
     P1OUT &=~ G_LED;                      // Turn OFF G_LED
 
     TACCR0 = 1000;                      // Set Timer Timeout Value
     TACCTL0 |= CCIE;                   // Enable Overflow Interrupt
     TACTL |=   MC_1                    // mode count up to CCR0
-             | TASSEL_1     // clock source A_clk
+             | TASSEL_2     // clock source sm_clk
              | TACLR        //timer A counter clear
-             |  ID_2;       //divides A clock by 1,2,4 or 8 if 0,1,2,or 3 respectively
+             |  ID_0;       //divides A clock by 1,2,4 or 8 if 0,1,2,or 3 respectively
 
-    BCSCTL3 |= LFXT1S_2;    //sets clock to internal oscillator (VLO)
-    __bis_SR_register(LPM3_bits + GIE); // Goto LPM3 (Only ACLK active), Enable CPU Interrupt
+    //BCSCTL3 |= LFXT1S_2;    //sets clock to internal oscillator (VLO)
+    __bis_SR_register(GIE); // Goto LPM3 (Only ACLK active), Enable CPU Interrupt
 }
 
 
